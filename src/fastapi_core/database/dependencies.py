b66d2f926f96b64eab97ec1d7b4eab66a8d87fa5
Damir.Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2023-11-08 00:54:40 +0300

    update database dependencies

--- src/fastapi_core/database/dependencies.py
+++ src/fastapi_core/database/dependencies.py
@@ -1,22 +1,30 @@
 from typing import AsyncGenerator, Callable
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 
 from fastapi_core.database.sessions import get_session_factories, get_engines
 from fastapi_core.settings.database import DatabaseSettings
 
 db_settings = DatabaseSettings()
 
-create_session, create_async_session = get_session_factories(
-    *get_engines(db_settings.DATABASE_URL.replace("+asyncpg", ""), db_settings.DATABASE_URL))
+
+def overridable_get_session_factories():
+    return get_session_factories(
+        *get_engines(db_settings.DATABASE_URL.replace("+asyncpg", ""), db_settings.DATABASE_URL)
+    )
 
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_session(
+    session_factories: tuple[Callable[[], Session], Callable[[], AsyncSession]] = Depends(
+        overridable_get_session_factories
+    )
+) -> AsyncGenerator[AsyncSession, None]:
     """
     Session dependency async generator
-    :param async_session_factory: async session factory (sessionmaker)
-    :return: AsyncSession object
     """
+    _, create_async_session = session_factories
     session = create_async_session()
     try:
         yield session
