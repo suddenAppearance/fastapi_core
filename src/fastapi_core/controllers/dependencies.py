ba596f90f436d3a2b89082b9bc5220cbddf727a0
Damir.Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2023-11-09 00:13:16 +0300

    controller deps fixes

--- src/fastapi_core/controllers/dependencies.py
+++ src/fastapi_core/controllers/dependencies.py
@@ -1,12 +1,14 @@
 import logging
-from logging import Logger
 from typing import Type, TypeVar, Callable
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 
-from fastapi_core.database.dependencies import get_session
+try:
+    from fastapi_core.database.dependencies import get_session
+except ImportError:
+    get_session = NotImplemented
 
 T = TypeVar("T")
 
@@ -14,19 +16,10 @@ T = TypeVar("T")
 def get_service(service: Type[T], db=True) -> Callable[..., T]:
     logger = logging.getLogger(service.__name__)
 
-    def _get_service_with_db_session(
-        request: Request,
-        session: AsyncSession = Depends(get_session),
-        logger: Logger = logger,
-    ):
-        return service(
-            session=session, logger=logger, container=request.state, headers=request.headers
-        )
+    def _get_service_with_db_session(request: Request, session: AsyncSession = Depends(get_session)):
+        return service(session=session, logger=logger, container=request.state, headers=request.headers)
 
-    def _get_service_without_db_session(
-        request: Request,
-        logger: Logger = logger,
-    ):
+    def _get_service_without_db_session(request: Request):
         return service(logger=logger, container=request.state, headers=request.headers)
 
     return _get_service_with_db_session if db else _get_service_without_db_session
