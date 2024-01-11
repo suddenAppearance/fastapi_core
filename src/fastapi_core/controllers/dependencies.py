41efdb9298341f37c820f3821ab2fc66a1ce7c00
Damir Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2024-01-11 19:09:57 +0300

    fix error

--- src/fastapi_core/controllers/dependencies.py
+++ src/fastapi_core/controllers/dependencies.py
@@ -1,17 +1,14 @@
 import logging
 from typing import Type, TypeVar, Callable
 
-from fastapi import Depends
 from starlette.requests import Request
 
 from fastapi_core.services.base import BaseServiceWithSession
 
 try:
     from sqlalchemy.ext.asyncio import AsyncSession
-    from fastapi_core.database.dependencies import get_session
 
 except ImportError:
-    get_session = NotImplemented
     AsyncSession = NotImplemented
 
 T = TypeVar("T")
@@ -22,8 +19,8 @@ def get_service(service: Type[T]) -> Callable[..., T]:
 
     db = issubclass(service, BaseServiceWithSession)
 
-    def _get_service_with_db_session(request: Request, session: AsyncSession = Depends(get_session)):
-        return service(session=session, logger=logger, container=request.state, headers=request.headers)
+    def _get_service_with_db_session(request: Request):
+        return service(session=request.state.session, logger=logger, container=request.state, headers=request.headers)
 
     def _get_service_without_db_session(request: Request):
         return service(logger=logger, container=request.state, headers=request.headers)
