868b7919f7b14dc0cbb861baf105beb646afaf41
Damir.Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2024-01-13 23:17:07 +0300

    add paginated request

--- src/fastapi_core/schemas/base.py
+++ src/fastapi_core/schemas/base.py
@@ -1,15 +1,25 @@
-from typing import TypeVar, Annotated
+from typing import TypeVar, Annotated, Literal
 
 from pydantic import BaseModel, Field
 
 from fastapi_core.settings.app import APISettings
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 class PaginatedRequestSchema(BaseModel):
     page: Annotated[int, Field(ge=1)]
     page_size: Annotated[int, Field(ge=1)]
+
+    def to_limit_offset(self, mode: Literal["dict", "tuple"] = "dict") -> dict[str, int] | tuple[int, int]:
+        limit = self.page_size
+        offset = (self.page - 1) * self.page_size
+
+        match mode:
+            case "dict":
+                return dict(limit=limit, offset=offset)
+            case "tuple":
+                return limit, offset
 
 
 class PaginatedResponseSchema(BaseModel):
