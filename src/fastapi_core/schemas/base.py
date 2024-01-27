5027e22525139e7800a2033abbf87e26ee802ae9
Damir.Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2024-01-27 20:17:42 +0300

    add token pagination

--- src/fastapi_core/schemas/base.py
+++ src/fastapi_core/schemas/base.py
@@ -23,3 +23,13 @@ class PaginatedRequestSchema(BaseModel):
 class PaginatedResponseSchema(BaseModel, Generic[T]):
     count: int
     items: list[T]
+
+
+class TokenPaginatedRequestSchema(BaseModel):
+    page_token: str | None
+    page_size: Annotated[int, Field(ge=1)]
+
+
+class TokenPaginatedResponseSchema(BaseModel, Generic[T]):
+    next_page_token: str | None
+    items: list[T]
