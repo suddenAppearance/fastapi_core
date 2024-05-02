db15db1f3eaca55defbde996b2c86e5a6e7ef860
Damir Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2024-05-02 11:35:32 +0300

    extend logging

--- src/fastapi_core/logging/configure.py
+++ src/fastapi_core/logging/configure.py
@@ -1,7 +1,26 @@
+import logging
+import sys
 from logging import Handler
 
 from loguru import logger
+from loguru._defaults import LOGURU_FORMAT
+
+from fastapi_core.logging.handlers import InterceptHandler
 
 
-def configure_logger(sink: Handler):
-    logger.add(sink)
+def configure_logger(sink: Handler | None = None):
+    sink = sink or InterceptHandler()
+
+    loggers = (
+        logging.getLogger(name)
+        for name in logging.root.manager.loggerDict
+        if name.startswith("uvicorn.")
+    )
+    for uvicorn_logger in loggers:
+        uvicorn_logger.handlers = []
+
+    logging.getLogger("uvicorn").handlers = [sink]
+
+    logger.configure(
+        handlers=[{"sink": sys.stderr, "level": logging.DEBUG, "format": LOGURU_FORMAT}]
+    )
