e6cbbfae5519aa34cca3a6bfe37264fdb9186fc7
Damir Kuzovatov <Damir.Kuzovatov@waveaccess.global>
2024-05-17 10:34:00 +0300

    update duplicate logger issues

--- src/fastapi_core/logging/configure.py
+++ src/fastapi_core/logging/configure.py
@@ -11,15 +11,16 @@ from fastapi_core.logging.handlers import InterceptHandler
 def configure_logger(sink: Handler | None = None):
     sink = sink or InterceptHandler()
 
+    # This makes only uvicorn.access and uvicorn.error loggers to have loguru sinks
+    # Thus avoiding duplicate log messages due to root uvicorn logger logging propagated logs from .access and .error
     loggers = (
         logging.getLogger(name)
         for name in logging.root.manager.loggerDict
-        if name.startswith("uvicorn.")
+        if name.startswith("uvicorn")
     )
     for uvicorn_logger in loggers:
         uvicorn_logger.handlers = [sink]
-
-    logging.getLogger("uvicorn.access").handlers = [sink]
+    logging.getLogger("uvicorn").handlers = []
 
     logger.configure(
         handlers=[{"sink": sys.stderr, "level": logging.DEBUG, "format": LOGURU_FORMAT, "enqueue": True}]
